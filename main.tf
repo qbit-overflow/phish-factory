bfa0093edc84adc25f81d79a5e51ad35dfe55501
Samuel Jones <qbit_rainbows@protonmail.com>
2021-08-15 17:23:58 -0400

    Removed Docker Feature
    Removed Docker Feature

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = ">= 2.13.0"
-    }
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.65"
@@ -18,8 +13,6 @@ terraform {
     }
   }
 }
-
-
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
@@ -36,17 +29,4 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "eastus"
   resource_group_name = azurerm_resource_group.rg.name
-}
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
 }
